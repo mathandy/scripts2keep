254504256b679735ee1c1905cfe7ebc06ddabe0f
Andrew Port <andyaport@gmail.com>
2021-08-10 21:13:05 -0700

    added support for old versions of tmux

--- flash.sh
+++ flash.sh
@@ -1,11 +1,48 @@
 #!/bin/bash
 # script to make terminal flash until any key is pressed
 
+# credit:
+# https://stackoverflow.com/questions/25532773/change-background-color-of-active-or-inactive-pane-in-tmux/33553372#33553372
+# 
+
+# check if tmux version is < 2.1
+tmux_is_old=$(awk -v n1="$(tmux -V| cut -d' ' -f2)" -v n2="2.1" 'BEGIN {printf (n1<n2?"1":"0")}')
+
+if [ $tmux_is_old ]; then
+    tmux_default=$(tmux show-options -g pane-border-style| cut -d' ' -f2)
+else
+    # NOTE -- I'm not sure how to retrieve the current setting, please help
+    tmux_default=default
+fi
+
+flashon () {
+    if [[ "$TERM" =~ "screen".* ]]; then
+        if [ $tmux_is_old ]; then
+            tmux set -g pane-border-style 'fg=colour235,bg=colour200' > /dev/null 2>&1
+        else
+            tmux select-pane -P 'fg=blue,bg=green' > /dev/null 2>&1
+        fi
+    else 
+        printf '\e[?5h'
+    fi
+}
+flashoff () {
+    if [[ "$TERM" =~ "screen".* ]]; then 
+        if [ $tmux_is_old ]; then
+            tmux set -g pane-border-style "$tmux_default" > /dev/null 2>&1
+        else
+            tmux select-pane -P "$tmux_default"
+        fi
+    else 
+        printf '\e[?5l'
+    fi
+}
+
 set -e
 trap ctrl_c INT
 
 function ctrl_c() {
-    printf '\e[?5l'
+    flashoff
 }
 
 if [ -t 0 ]; then
@@ -16,9 +53,9 @@ fi
 count=0
 keypress=''
 while [ "x$keypress" = "x" ]; do
-    printf '\e[?5h'
+    flashon
     sleep 0.5
-    printf '\e[?5l'
+    flashoff
     sleep 0.5
     keypress="`cat -v`"
 done
